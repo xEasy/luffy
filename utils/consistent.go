bed5b60ec4e4e6ad2151dab9898fa05dafa55f6e
xEasy <yijo1001@gmail.com>
2022-05-30 14:17:33 +0800

    [feature] consistent hash
    TODO using consistent hash to assgin message to pool ?

---
+++ utils/consistent.go
@@ -0,0 +1,132 @@
+package utils
+
+import (
+	"errors"
+	"hash/crc32"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+// Consistent hashing
+type Consistent struct {
+	sync.RWMutex                          // map RWMutex lock
+	hashSortNodes     []uint32            // sorted hash virtual nodes
+	circle            map[uint32]string   // virtual nodes info
+	nodes             map[string]bool     //binding nodes
+	nodeVirtuals      map[string][]uint32 // node's virtual nodes
+	virtualNodesCount int
+}
+
+func NewConsistent() *Consistent {
+	return &Consistent{}
+}
+
+func (c *Consistent) Add(node string, virtualNodeCount int) error {
+	if node == "" {
+		return nil
+	}
+	c.Lock()
+	defer c.Unlock()
+
+	if c.circle == nil {
+		c.circle = make(map[uint32]string)
+	}
+
+	if c.nodes == nil {
+		c.nodes = make(map[string]bool)
+	}
+
+	if c.nodeVirtuals == nil {
+		c.nodeVirtuals = make(map[string][]uint32)
+	}
+
+	if _, ok := c.nodes[node]; ok {
+		return errors.New("node existed!")
+	}
+	c.nodes[node] = true
+	c.nodeVirtuals[node] = make([]uint32, virtualNodeCount)
+
+	// add virtual nodes
+	for i := 0; i < virtualNodeCount; i++ {
+		virtualKey := c.hashKey(strings.Join([]string{node, strconv.Itoa(i)}, "-"))
+		c.circle[virtualKey] = node
+		c.nodeVirtuals[node][i] = virtualKey
+		c.hashSortNodes = append(c.hashSortNodes, virtualKey)
+	}
+
+	// sort virtual nodes
+	sort.Slice(c.hashSortNodes, func(x, y int) bool {
+		return c.hashSortNodes[x] < c.hashSortNodes[y]
+	})
+
+	return nil
+}
+
+func (c *Consistent) GetNode(key string) string {
+	c.RLock()
+	defer c.RUnlock()
+
+	hash := c.hashKey(key)
+	i := c.getPosition(hash)
+
+	return c.circle[i]
+}
+
+func (c *Consistent) Remove(node string) error {
+	if node == "" {
+		return nil
+	}
+
+	c.Lock()
+	defer c.Unlock()
+
+	if _, ok := c.nodes[node]; !ok {
+		return errors.New("node not existed!")
+	} else {
+		delete(c.nodes, node)
+	}
+
+	// remove virtual nodes from sortedSlice
+	nodeVirtuals := c.nodeVirtuals[node]
+	index := 0
+	for i := 0; i < len(c.hashSortNodes); i++ {
+		nodeKey := c.hashSortNodes[i]
+		if c.circle[nodeKey] != node {
+			continue
+		}
+		c.hashSortNodes[index] = c.hashSortNodes[i]
+		index++
+	}
+	c.hashSortNodes = c.hashSortNodes[:index]
+
+	// delete virtual nodes form circle
+	for _, v := range nodeVirtuals {
+		delete(c.circle, v)
+	}
+
+	delete(c.nodeVirtuals, node)
+
+	return nil
+}
+
+func (c *Consistent) hashKey(key string) uint32 {
+	return crc32.ChecksumIEEE([]byte(key))
+}
+
+func (c *Consistent) getPosition(hash uint32) uint32 {
+	nodesLen := len(c.hashSortNodes)
+	i := sort.Search(nodesLen, func(i int) bool {
+		return c.hashSortNodes[i] >= hash
+	})
+	if i < nodesLen {
+		if i == nodesLen-1 {
+			i = 0
+		}
+	} else {
+		i = nodesLen - 1
+	}
+
+	return c.hashSortNodes[i]
+}
---
+++ utils/consistent_test.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func TestConsistent(t *testing.T) {
+	cs := NewConsistent()
+	cs.Add("one", 150)
+	cs.Add("two", 100)
+
+	err := cs.Add("one", 1)
+	if err == nil {
+		t.Fatal("add existed node!")
+	}
+
+	node := cs.GetNode("hello")
+	if node == "" {
+		t.Fatal("get empty node info!")
+	}
+	t.Logf("get node success, node = %s, key = %s", node, "hello")
+
+	err = cs.Remove("one")
+	if err != nil {
+		t.Logf("remove node <one> fail!")
+	}
+
+	if len(cs.hashSortNodes) != 100 {
+		t.Logf("remove node wrong")
+	}
+	if len(cs.circle) != 100 {
+		t.Logf("remove node wrong")
+	}
+
+	node = cs.GetNode("hello")
+	if node != "two" {
+		t.Logf("Get node wrong")
+	}
+
+}
+
+func BenchmarkConsistent(t *testing.B) {
+	// test 10 nodes with 100 vitual nodes
+	cs := NewConsistent()
+	for i := 0; i < 100; i++ {
+		cs.Add(fmt.Sprintf("node:%d", i), 100)
+	}
+
+	t.ResetTimer()
+	for i := 0; i < t.N; i++ {
+		cs.GetNode("call:" + strconv.Itoa(i))
+	}
+}
